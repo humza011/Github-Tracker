61c4919d952d48e0679e482a49be64ad0514c818
Humza Hasan <humzahasan2101@gmail.com>
2020-03-25 23:58:32 +0530

    Added alert for search

--- src/App.js
+++ src/App.js
@@ -4,14 +4,15 @@ import React, { Component } from 'react';
 import Navbar from './components/layout/Navbar'
 import Users from './components/users/Users'
 import Search from './components/users/Search'
-
+import Alert from './components/layout/Alert'
 import axios from 'axios'
 import './App.css'
 
 class App extends Component {
   state = {
     loading : false,
-    users : []
+    users : [],
+    alert :null
   }
 
   async componentDidMount() {
@@ -30,12 +31,26 @@ class App extends Component {
 
   clearUser = () => { this.setState({users:[] , loading : false})}
 
+  setAlert = (msg , type) => {
+    this.setState({alert : {msg : msg, type : type}})
+
+    setTimeout(() => {
+      this.setState({alert : null})
+    }, 5000);
+  } 
+
   render() {
     return (
       <div className='App'>
         <Navbar title="Github Finder" icon="fab fa-github"/> 
         <div className="container">
-          <Search searchUser={this.searchUser} clearUser ={this.clearUser} showClear = {this.state.users.length > 0 ? true : false}/>
+          <Alert alert={this.state.alert}/>
+          <Search 
+          searchUser={this.searchUser} 
+          clearUser ={this.clearUser} 
+          showClear = {this.state.users.length > 0 ? true : false}
+          setAlert = {this.setAlert}
+          />
           <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,8 +1,16 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 
 export class Search extends Component {
     state = {
         text : ''
+    }
+
+    static propTypes = {
+        searchUser : PropTypes.func.isRequired,
+        clearUser : PropTypes.func.isRequired,
+        showClear : PropTypes.bool.isRequired,
+        setAlert : PropTypes.func.isRequired
     }
     
     onChange = (e) => {
@@ -11,8 +19,12 @@ export class Search extends Component {
 
     onSubmit = (e) => {
         e.preventDefault()
+        if(this.state.text === '') {
+            this.props.setAlert('Please enter user to search','danger')
+        } else {
         this.props.searchUser(this.state.text)
         this.setState({text : ''})
+        }
     }
 
     render() {
